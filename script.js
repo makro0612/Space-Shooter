13a3bb138457b9fc21dd19da5fce5b4324924524
makro0612 <martinkro07@gmail.com>
2025-02-05 14:08:50 +0100

    Bevegelse og sånn
    Jeg vil ikke skyte meg selv SÅ mye mer, bare litt

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-const canvas = document.getElementById("gamecanvas")
+const canvas = document.getElementById("gamecanvas");
 
-const HEIGHT = 500
-const WIDTH = 700
+const HEIGHT = 500;
+const WIDTH = 700;
 
-canvas.height = HEIGHT
-canvas.width = WIDTH
+canvas.height = HEIGHT;
+canvas.width = WIDTH;
 
-const ctx = canvas.getContext("2d") 
+const ctx = canvas.getContext("2d");
 const ship = document.getElementById("shipImg");
 
+function draw(x, y) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(ship, x, y);
+    requestAnimationFrame(() => draw(x, y));
+}
+shipxx = 0
 
-skip = ctx.drawImage(ship, (canvas.width/2)-35, 450);
 
+draw((canvas.width / 2) - 35, 450);
 
-/*max-width: 100%;
-    height: auto;
-    z-index: 100
-*/
+document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowLeft") {
+        shipxx = shipxx - 10
+        draw((canvas.width / 2) - 35 + shipxx, 450);
+        console.log(shipxx)
+    } else if (event.key === "ArrowRight") {
+        shipxx += 10
+        draw((canvas.width / 2) - 35 + shipxx, 450);
+    }
+    if(shipxx < -310){
+        console.log("Game Over")
+        canvas.style.backgroundColor = "red";
+    }
+
+});
+
