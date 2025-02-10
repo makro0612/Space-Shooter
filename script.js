528b0a06699cda5d91a40b6ddaf80e894e208ede
makro0612 <martinkro07@gmail.com>
2025-02-10 08:44:23 +0100

    Skyte ting ( kinda )
    Prøver å lage en måte å skyte på. Kan skyte et skudd, men bruker et object, så kan bare ha ett av gangen. Skal prøve å bruke arrays/lister

--- script.js
+++ script.js
@@ -1,37 +1,63 @@
 const canvas = document.getElementById("gamecanvas");
 
-const HEIGHT = 500;
-const WIDTH = 700;
+const HEIGHT = document.body.offsetHeight;
+const WIDTH = document.body.offsetWidth/3;
 
 canvas.height = HEIGHT;
 canvas.width = WIDTH;
 
 const ctx = canvas.getContext("2d");
-const ship = document.getElementById("shipImg");
 
-function draw(x, y) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(ship, x, y);
-    requestAnimationFrame(() => draw(x, y));
+const ship1 = {
+    elm: document.getElementById("shipImg"),
+    x: canvas.width / 2 - 25,
+    y: document.body.offsetHeight - 100,
+    bullets: []
 }
-shipxx = 0
+
+const bullet1 = {
+    elm: document.getElementById("bulletImg"),
+    x: ship1.x ,
+    y: ship1.y,
+    visible: false
+}
 
 
-draw((canvas.width / 2) - 35, 450);
+
+function drawShip(ship) {
+    ctx.drawImage(ship.elm, ship.x, ship.y);
+}
+
+function drawBullet(bullet) {
+    if (bullet.visible) {
+        ctx.drawImage(bullet.elm, bullet.x, bullet.y);
+        
+    }
+}
+
+function oppdater() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawShip(ship1);
+    drawBullet(bullet1);    
+    requestAnimationFrame(oppdater);
+    bullet1.y -= 15;
+}
+
+oppdater();
 
 document.addEventListener("keydown", function(event) {
     if (event.key === "ArrowLeft") {
-        shipxx = shipxx - 10
-        draw((canvas.width / 2) - 35 + shipxx, 450);
-        console.log(shipxx)
+        ship1.x -= 10;
     } else if (event.key === "ArrowRight") {
-        shipxx += 10
-        draw((canvas.width / 2) - 35 + shipxx, 450);
+        ship1.x += 10;
     }
-    if(shipxx < -310){
-        console.log("Game Over")
-        canvas.style.backgroundColor = "red";
+    else if (event.key === " ") {
+        bullet1.visible = true;
+        bullet1.x = ship1.x + 25;
+        bullet1.y = ship1.y;
     }
+    
 
 });
+
 
